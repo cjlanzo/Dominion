bbfbb68023b29b62b36b1deac79e39beb95d34fc
Chris <cjlanzo@ncsu.edu>
2016-07-20 17:36:05 -0400

    Initial Commit

---
+++ Dominion/DominionClient/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DominionClient
+{
+	public static class Client
+	{
+		public static void Main()
+		{
+			const string ip = "10.0.0.25";
+			const int port = 8001;
+
+			try
+			{
+				TcpClient tcpClient = new TcpClient();
+
+				Console.WriteLine(@"Connecting...");
+
+				tcpClient.Connect(ip, port);
+
+				Console.WriteLine(@"Connected");
+				Console.Write(@"Enter the string to be transmitted: ");
+
+				string input = Console.ReadLine();
+				Stream stream = tcpClient.GetStream();
+
+				ASCIIEncoding asen = new ASCIIEncoding();
+				byte[] ba = asen.GetBytes(input);
+
+				Console.WriteLine(@"Transmitting");
+
+				stream.Write(ba, 0, ba.Length);
+
+				byte[] bb = new byte[100];
+
+				int k = stream.Read(bb, 0, 100);
+
+				for (int i = 0; i < k; i++)
+				{
+					Console.Write(Convert.ToChar(bb[i]));
+				}
+
+				tcpClient.Close();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.StackTrace);
+			}
+		}
+	}
+}
---
+++ Dominion/DominionServer/Server.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DominionServer
+{
+	public static class Server
+	{
+		public static void Main()
+		{
+			try
+			{
+				IPAddress ipAddress = IPAddress.Parse("10.0.0.25");
+
+				const int port = 8001;
+				TcpListener tcpListener = new TcpListener(ipAddress, port);
+				tcpListener.Start();
+
+				Console.WriteLine($"The server is running at port {port}");
+				Console.WriteLine($"The local End Point is {tcpListener.LocalEndpoint}");
+				Console.WriteLine(@"Waiting for a connection...");
+
+				Socket socket = tcpListener.AcceptSocket();
+
+				Console.WriteLine($"Connection accepted from {socket.RemoteEndPoint}");
+
+				byte[] bytes = new byte[100];
+				int k = socket.Receive(bytes);
+
+				Console.WriteLine(@"Received...");
+
+				for (int i = 0; i < k; i++)
+				{
+					Console.Write(Convert.ToChar(bytes[i]));
+				}
+
+				ASCIIEncoding asen = new ASCIIEncoding();
+				socket.Send(asen.GetBytes(@"The string was received by the server."));
+
+				Console.WriteLine(@"\nSent Acknowledgement");
+
+				socket.Close();
+				tcpListener.Stop();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.StackTrace);
+			}
+		}
+	}
+}
